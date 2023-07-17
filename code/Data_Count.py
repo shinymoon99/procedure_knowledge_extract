35e5a564c6dfefb4c24da11aa859b60f76a60c28
shinymoon99 <2686288339@qq.com>
2023-07-17 15:59:04 +0800

    add dataset_info_gen

--- code/Data_Count.py
+++ code/Data_Count.py
@@ -9,17 +9,31 @@ def get_word_num(sentence):
     # Count the number of words
     word_num = len(words)
     return word_num
+#动作数量
+cnt  = []
 with open('D:\pycode\MGTC\data\data_correct_formated.json', encoding='utf-8') as f:
     datas = json.load(f)
-    for data in datas:
-        cnt = cnt + len(data['labels'])
-
+cnt_all = 0
+for data in datas:
+    cnt_all = cnt_all + len(data['labels'])
+    cnt.append(data["labels"])
 #句子平均词数
 with open('D:\pycode\MGTC\data\data_correct_formated.json', encoding='utf-8') as f:
     datas = json.load(f)
-#句子含有动作的数量
-
-#句子含有语义角色的种类
+word_num = []
+for data in datas:
+    t = get_word_num(data["sentence"])
+    word_num.append(t)        
+#命题含有语义角色的种类
+sr = []
+t = {"A0":0,"A1":0,"A2":0,"ADV":0,"TMP":0,"CND":0,"PRP":0,"MNR":0}
+for sentence in datas:
+    for pro in sentence["labels"]:
+        for arg in pro["ARG"]:
+            t[arg]+=1
+        for argm in pro["ARGM"]:
+            t[argm]+=1
+    sr.append(t)
 
 print(cnt)
 print(len(datas))
